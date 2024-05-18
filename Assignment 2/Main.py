275b003fa4f91e94d63f6c2fbe33a8d4f0376682
MarijnBaas <m.baas@student.tue.nl>
2024-05-18 14:47:22 +0200

    Deciding actions

--- Assignment 2/Main.py
+++ Assignment 2/Main.py
@@ -1,3 +1,5 @@
+import random
+
 def read_file(input_file):
     """
     Opens the file of the input and reads the contents and divides it into lists 
@@ -27,40 +29,45 @@ def read_file(input_file):
     
 
 class cell_type:
-    def __init__(self, move, location):
+    def __init__(self, move, kill, growth, location):
         self.move = move
+        self.kill = kill
+        self.growth = growth
         self.location = location
 
     def move_cell(location):
         True;
 
-    def generate_action(move, kill, growth):
-        True;
+    def generate_action(self, probability, move, kill, growth):
+        action = 0
+        if probability <= move/(move+kill+growth):          #0.5/0.3
+            action = 'move'
+        elif probability <= (move+kill)/(move+kill+growth): #1/1
+            action = 'kill'
+        elif probability > (move+kill)/(move+kill+growth):
+            action = 'growth'
+        else:
+            action = 'rest'
+        return action
+
+    #def get_move_squares(self, location):
+
+
 
 
 class Infected(cell_type):      #mobility, location, growth
-    def __init__(self, move, location, growth):
-         super().__init__(move,location)
-         self.growth = growth
-    
-    def duplicate_cell(location):
-        True;
+    def __init__(self, move, kill, growth, location):
+         super().__init__(move, kill, growth, location)
 
-class MCell(cell_type):
-    def __init__(self, move, location, kill):
-         super().__init__(move,location)
-         self.kill = kill
-        
-    def kill_cell(location):    #check if there is a I cell in surrounding blocks and then 
-        True;
+class MCell(cell_type):      #mobility, location, growth
+    def __init__(self, move, kill, growth, location):
+         super().__init__(move, kill, growth, location)
 
-class TCell(cell_type):
-    def __init__(self, move, location, kill):
-         super().__init__(move,location)
-         self.kill = kill
+class TCell(cell_type):      #mobility, location, growth
+    def __init__(self, move, kill, growth, location):
+         super().__init__(move, kill, growth, location)
         
-    def kill_cell(location):
-        True;
+
 
 def order_of_operations(grid_size, grid_buildup):
     cell_locations = []
@@ -74,21 +81,31 @@ def order_of_operations(grid_size, grid_buildup):
 
 
 def simulation(input_file, output_file):
+    #Extract all data from input file
     I_Parameters,M_Parameters,T_Parameters,grid_size,grid_buildup,number_of_iterations,seed = read_file(input_file)
-
-    cell_locations = order_of_operations(grid_size, grid_buildup)
-
-    for i in range(1): #number_of_iterations
+    for i in range(1):     #loop over all iterations #number_of_iterations    
+        #get all locations with a cell in them
+        cell_locations = order_of_operations(grid_size, grid_buildup)
+        #generate random number for probability calculations
+        probability = random.random()
         for cells in range(len(cell_locations)):
-                row = cell_locations[cells][0]
-                column = cell_locations[cells][1]
-                current_cell = grid_buildup[row][column]
-                print(current_cell)
-                if current_cell == 'I':
-                    cell = Infected(I_Parameters[0], [row, column],I_Parameters[2])
-                elif current_cell == 'T':
-                    cell = TCell(T_Parameters[0], [row, column], T_Parameters[1])
-                else:
-                    cell = MCell(M_Parameters[0], [row, column], M_Parameters[1])
+            #Check cell location and see what cell type is inside of them
+            row = cell_locations[cells][0]
+            column = cell_locations[cells][1]
+            current_cell = grid_buildup[row][column]
+            if current_cell == 'I':
+                cell = Infected(I_Parameters[0], I_Parameters[1],I_Parameters[2], [row, column])
+            elif current_cell == 'T':
+                cell = TCell(T_Parameters[0], T_Parameters[1], T_Parameters[2], [row, column])
+            else:
+                cell = MCell(M_Parameters[0], M_Parameters[1], M_Parameters[2], [row, column])
+            #See what action the cell will do depending on the probability and cell parameters
+            action = cell.generate_action(probability, cell.move, cell.kill, cell.growth)
+            print(action)
+            
+
+            
+                
+                
 
 simulation('initial_configuration1.txt','final_configuration.txt')
